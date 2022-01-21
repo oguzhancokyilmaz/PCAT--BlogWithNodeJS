0d542f54cddd8f244861aada69a4a1e4a5ce1d2f
oguzhancokyilmaz <ocokyilmaz@gmail.com>
2022-01-22 02:09:38 +0300

    You can add photos for listing homepage

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
+const mongoose = require ('mongoose')
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo')
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true})) //urldeki datayı okumamızı sağlar
+app.use(express.json()) // Yukardaki ve bu body parser için kullanılıyormuş. Bu ikisini yazdığımızda formdan gönderdiğimiz veriyi req.body ile yakalayıp konsola yazdırabildik. Bunlar middleware. datayı json formatına döndürmeyi sağlar
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/',async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos // bu aynı zamanda photos: photos komutudur veritabanından aldık photo verilerini index.ejs ye yolladık
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 const port = 3000;
